964101c88800f002f2fb4768113f0de819a49215
Tanvi <tanviijanday@gmail.com>
2026-05-09 10:29:40 +0530

    Day 4

--- app/main.py
+++ app/main.py
@@ -1,24 +1,40 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
 import random
 
 app = FastAPI()
 
-# Home endpoint
+# Prometheus metric
+REQUEST_COUNT = Counter(
+    "app_requests_total",
+    "Total number of requests"
+)
+
 @app.get("/")
 def home():
+    REQUEST_COUNT.inc()
     return {"message": "AutoOps API is running 🚀"}
 
-
-# Health check endpoint (VERY IMPORTANT)
 @app.get("/health")
 def health():
+    REQUEST_COUNT.inc()
     return {"status": "healthy"}
 
-
-# Failure simulation endpoint (VERY IMPORTANT)
 @app.get("/simulate-failure")
 def simulate_failure():
+    REQUEST_COUNT.inc()
+
     if random.choice([True, False]):
         return {"status": "working fine"}
     else:
         raise Exception("Simulated failure 😈")
+
+
+# Metrics endpoint for Prometheus
+@app.get("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        media_type="text/plain"
+    )
